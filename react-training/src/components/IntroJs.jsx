00ad05c51995dcd4303acca87b7d71d55af4c2fd
JayRathod123 <Jay.Rathod.Micra@gmail.com>
2024-09-11 22:26:18 +0530

    intro to Javascript
    intro to Javascript write first program

---
+++ react-training/src/components/IntroJs.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const IntroJs = () => {
+    console.log("Hello world")
+
+
+// String
+const name = "Alice";
+console.log("String:", name); // Output: String: Alice
+
+// Number
+const age = 25;
+console.log("Number:", age); // Output: Number: 25
+
+// Boolean
+const isStudent = true;
+console.log("Boolean:", isStudent); // Output: Boolean: true
+
+// Null
+const emptyValue = null;
+console.log("Null:", emptyValue); // Output: Null: null
+
+// Undefined
+let uninitialized;
+console.log("Undefined:", uninitialized); // Output: Undefined: undefined
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default IntroJs
